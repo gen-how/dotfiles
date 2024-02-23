acc8740e8b704a33b7edaa2bb2ca902b9c6fa9b9
gen-how <billchen85@gmail.com>
2024-02-23 23:22:11 +0800

    Add script to deploy dotfiles
    The script use rsync command to copy changed
    dotfiles to XDG Base Directory.
    https://specifications.freedesktop.org/basedir-spec/basedir-spec-0.6.html
    
    $XDG_CONFIG_HOME default to "$HOME/.config".

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+# Gets the directory of the script.
+dotfiles_dir=$(dirname $0)
+
+# Use the rsync command to copy the dotfiles to the XDG_CONFIG_HOME directory.
+echo "Deploying dotfiles to \"${XDG_CONFIG_HOME}\""
+rsync -auv ${dotfiles_dir}/.config/ ${XDG_CONFIG_HOME}
