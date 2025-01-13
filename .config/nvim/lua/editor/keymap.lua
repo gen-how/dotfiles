b824f44d8f4bf8913b4f7f4d56dad7d8792ebcca
gen-how <billchen85@gmail.com>
2025-01-13 15:14:29 +0800

    update config for "neovim"
    Add some keymaps for vscode-neovim extension.
    
    Add "lazy-lock.json" to source control.

--- .config/nvim/lua/editor/keymap.lua
+++ .config/nvim/lua/editor/keymap.lua
@@ -11,3 +11,26 @@ vim.keymap.set('n', ']d', function()
     end,
     { desc = 'Go to next [D]iagnostic message in VSCode' }
 )
+
+vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
+        vscode.call('workbench.view.explorer')
+    end,
+    { desc = 'Open [F]ile Explorer in VSCode' }
+)
+
+vim.keymap.set({ 'n', 'v' }, '<leader>v', function()
+        vscode.call('workbench.action.openView')
+    end,
+    { desc = 'Open [V]iew in VSCode' }
+)
+
+vim.keymap.set({ 'n', 'v' }, '<leader>g', function()
+        vscode.call('workbench.view.scm')
+    end,
+    { desc = 'Open [G]it in VSCode' }
+)
+
+-- Quick search & replace keymaps, the <Left> only works in native Neovim.
+vim.keymap.set('n', '<leader>s', ':%s:\\C::gc')
+vim.keymap.set('n', '<leader><leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI')
+vim.keymap.set('v', '<leader>s', '"sy:%s:<C-r>s:<C-r>s:g')
--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -4,7 +4,27 @@
 -- See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Delete character withtout save to register
+vim.keymap.set({ 'n', 'v' }, 'x', '"_x')
+
+-- Paste without overwriting the register
+vim.keymap.set('v', 'p', 'P')
+
+-- Indent without leaving visual mode
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Scroll the screen and center the cursor
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix lists' })
+
+-- Quick search & replace keymaps, the <Left> only works in native Neovim.
+vim.keymap.set('n', '<leader>s', ':%s:\\C::gc<Left><Left><Left><Left>')
+vim.keymap.set('n', '<leader><leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
+vim.keymap.set('v', '<leader>s', '"sy:%s:<C-r>s:<C-r>s:g<Left><Left>')
