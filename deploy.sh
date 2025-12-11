23f166514da4a0a04ef4ecbedbb39782ecd4be8e
gen-how <billchen85@gmail.com>
2025-12-11 23:18:28 +0800

    [homebrew] update script for upgrading packages
    Renamed from "brew.sh" to "brewup".

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,25 @@
-CYAN=$'\033[0;36m'
-BOLD=$'\033[1m'
-RST=$'\033[0m'
+RST=$(tput sgr0)  # Reset all attributes
+B=$(tput bold)    # Bold
+BU=$(tput smul)   # Begin underline
+EU=$(tput rmul)   # Exit underline
+RD=$(tput setaf 1)
+GN=$(tput setaf 2)
+YE=$(tput setaf 3)
+BL=$(tput setaf 4)
+MG=$(tput setaf 5)
+CY=$(tput setaf 6)
+WH=$(tput setaf 7)
 
-log() { echo "${CYAN}==>${RST}${BOLD} $1${RST}"; }
+
+info() { echo "${BL}==> ${RST}${B}$1${RST}"; }
 
 # Gets the directory of the script.
 dotfiles_dir=$(dirname $0)
 
 # Use the rsync command to copy the dotfiles to the XDG_CONFIG_HOME directory.
-log "Deploying dotfiles to \"${XDG_CONFIG_HOME}\""
+info "Deploying dotfiles to \"${XDG_CONFIG_HOME}\""
 rsync -auv ${dotfiles_dir}/.config/ ${XDG_CONFIG_HOME}\
     --exclude "nvim"\
 
-log "Deploying dotfiles to \"${XDG_BIN_HOME}\""
+info "Deploying dotfiles to \"${XDG_BIN_HOME}\""
 rsync -auv ${dotfiles_dir}/.local/bin/ ${XDG_BIN_HOME}\
